4bc55fd3488dbfc4416b7be0eb56187693031e0b
John Climie <johnclimie00@gmail.com>
2021-12-27 12:36:16 -0500

    Lat and Long API now works

---
+++ assets/javascript.js
@@ -0,0 +1,17 @@
+// $.ajax({
+//     url: 'http://api.positionstack.com/v1/forward',
+//     data: {
+//         access_key: '1d78fafa76f422a476d2002242731baa',
+//         query: 'atlanta',
+//         limit: 1
+//     }
+// }).done(function (response) {
+//     // console.log(response.data[0].latitude);
+//     var lat = response.data[0].latitude;
+//     var long= response.data[0].longitude;
+//     console.log(lat);
+//     console.log(long);
+// })
+
+var tempLat = 33.76;
+var tempLong = -84.41;
