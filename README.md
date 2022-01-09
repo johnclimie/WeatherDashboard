71d2ee41b6aaa26fdf52be60e7c0850d91fb21b0
John Climie <johnclimie00@gmail.com>
2022-01-09 17:28:56 -0500

    Included comments and filled out README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-test
+# Weather Dashboard
+
+## Description
+
+In this project, I made a Weather Dashboard. The user would enter a location into the text box click search, and then the weather for that location will appear on the page.
+
+I made this project from blank files. I used HTML, Javascript, jQuery and Bootstrap for a clean and responsive look and feel.
+
+This project also includes two APIs, both hosted by OpenWeather, which provides weather information based on a certain location. The first API is called One Call API, which provides weather for a location based on the latitude and longitude inputted. In order to get the coordinates, I used another API called Geocoding API, which provided the coordinates for a location.
+
+This project was my most challenging yet, as it required two server-side API's in order to function. However, I found this project entertaining. There were many challenges while working on this project, such as GitHub pages not allowing the API to processes due to a content mix-up error, which required me to use a new API to get the coordinates. I also had to write several functions within functions. Overall, I felt like the project turned out well in the end.
+
+![](./assets/screenshot.png)
+
+**Link** 
+
+GitHub Pages: https://johnclimie.github.io/WeatherDashboard/
--- assets/javascript.js
+++ assets/javascript.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // Variables grab elements from index file
     var textInput = $('#textInput');
     var searchBtn = $('#searchBtn');
     var searchHistory = $('#searchHistory');
@@ -6,33 +7,40 @@ $(document).ready(function () {
     var clearBtn = $('#clearHistory');
 
     searchBtn.click(() => {
+        // If nothing is entered, the user is told to enter a locaiton, else, the funciton flows through
         if (textInput.val().length == 0) {
             window.alert("Please enter a location");
             return;
         } else {
+            // API gets coordinates based on the input in the textbox
             $.ajax({
                 url: `https://api.openweathermap.org/geo/1.0/direct?q=${textInput.val()}&limit=5&appid=5995eb989855fb7c98ae5e17f47586af`
             })
             .done(function (response) {
+
+                // Coordinates are stored in variables based on the response to the API call
                 var lat = response[0].lat;
                 var long = response[0].lon;
 
-                //Appends new button based on recent search
-
+                // Appends new button based on recent search
                 const button = $("<button>", {
                     type: "button",
                     "class": "prevSearch col-12 btn border-info m-1",
                     text: textInput.val().toLowerCase().split(' ').map((s) => s.charAt(0).toUpperCase() + s.substring(1)).join(' ')
                 }).appendTo(searchHistory);
 
-                localStorage.setItem("test", searchHistory.html());
+                // Stores prevois search in local storage
+                localStorage.setItem("history", searchHistory.html());
 
+                // API calls for weather information based on coordinates
                 $.ajax({
                     type: 'POST',
                     url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,minutely,alerts&appid=5995eb989855fb7c98ae5e17f47586af&units=imperial`,
                     dataType: "json"
                 }).done(function (response) {
+                    // Empties search results after each click
                     searchResults.empty();
+                    // Sets background of UV Index based on the value provided
                     var uvColor;
                     if (response.current.uvi < 3) {
                         uvColor = "bg-success";
@@ -42,6 +50,7 @@ $(document).ready(function () {
                         uvColor = "bg-danger";
                     }
 
+                    // Appends Search results with information provided by API
                     searchResults.append(
                         `<div class="p-3 border border-dark">
                         <img src ="https://openweathermap.org/img/wn/${response.current.weather[0].icon}@2x.png" alt="Icon of weather condition">
@@ -60,7 +69,7 @@ $(document).ready(function () {
                     )
 
                     var forecast = $("#forecast");
-
+                    // Appends forecast for the next 5 days
                     for (var i = 1; i < 6; i++) {
                         var date = new Date(response.daily[i].dt * 1000);
                         var months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
@@ -79,10 +88,10 @@ $(document).ready(function () {
         }    
     })
 
+    // Gets local storage for previous searches
+    searchHistory.html(localStorage.getItem('history'));
 
-    searchHistory.html(localStorage.getItem('test'));
-
-
+    // Grabs city from previous search button that is clicked, and appends weather information based on the previous search, similar to search button function
     searchHistory.on("click", ".prevSearch", function() {
         var buttonCity = $(this).text()
 
@@ -143,6 +152,7 @@ $(document).ready(function () {
         })
     })
 
+    // Clears search history, which helps for testing
     clearBtn.click(function () {
         localStorage.clear();
         location.reload();
